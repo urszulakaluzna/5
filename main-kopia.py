88aa27f6e24524c5ccf6e94e1c3a99595e16f987
urszulakaluzna <51214965+urszulakaluzna@users.noreply.github.com>
2020-03-15 00:23:17 +0100

    Add files via upload

---
+++ main-kopia.py
@@ -0,0 +1,8 @@
+import datetime
+
+name = input("Hej what is Your name? ")
+age = input("Now tell me how old are You ")
+a = 100 - int(age)
+b = a + 2020
+print(name + " You will be hundret years old in " + str(b),".")
+
